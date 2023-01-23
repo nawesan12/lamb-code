d7da8bab13ffb091c07d675c3ad3f5d274febeb4
Nahuel Santillan <84944031+nawesan12@users.noreply.github.com>
2023-01-23 02:16:08 -0300

    Update hello.ts

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,8 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-type Data = {
-  name: string
+export default function handler(_: NextApiRequest, res: NextApiResponse<string>) {
+  const status: string = "🟢 All systems stable" 
+
+  res.status(200).json(status)
 }
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
-}
