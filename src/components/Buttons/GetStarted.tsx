e10fa13287a63b6d1df53ee0934ab2328a64a58b
Nahuel Santillan <nahueltecnicomdp@gmail.com>
2023-01-24 18:03:46 -0300

    SEO and Mockup

--- src/components/Buttons/GetStarted.tsx
+++ src/components/Buttons/GetStarted.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link"
 
 export default function GetStartedBtn() {
-  return <Link href="/" className="bg-black text-white font-semibold rounded-md px-4 py-2 shadow-black shadow-md">Get Started</Link>
+  return <Link href="/auth/register" className="bg-black text-white font-semibold rounded-md px-9 py-2 shadow-black shadow-md">Get Started</Link>
 }
--- src/components/Home/FAQ.tsx
+++ src/components/Home/FAQ.tsx
@@ -5,76 +5,41 @@ export default function HomeFAQ() {
     <section className="section">
       <SectionTitle title="FAQ" />
       <div className="container px-6 py-12 mx-auto">
+        <div className="grid grid-cols-1 gap-8 mt-8 md:grid-cols-2 xl:grid-cols-2">
+        
+        <div>
+            <h1 className="text-xl font-semibold text-gray-700">What can i expect at my first consultation?</h1>
 
-        <div className="grid grid-cols-1 gap-8 mt-8 lg:mt-16 md:grid-cols-2 xl:grid-cols-2">
-            <div>
-                <div className="inline-block p-3 text-white bg-black rounded-lg">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-                    </svg>
-                </div>
-
-                <div>
-                    <h1 className="text-xl font-semibold text-gray-700">What can i expect at my first consultation?</h1>
-
-                    <p className="mt-2 text-sm text-gray-500">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident placeat, consequatur eveniet veritatis quos dignissimos beatae dolores exercitationem laboriosam officia magnam atque blanditiis illum doloremque magni ex corrupti tempora quis.
-                    </p>
-                </div>
-            </div>
-
-            <div>
-                <div className="inline-block p-3 text-white bg-black rounded-lg">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-                    </svg>
-                </div>
-
-                <div>
-                    <h1 className="text-xl font-semibold text-gray-700">What are your opening house?</h1>
-
-                    <p className="mt-2 text-sm text-gray-500 ">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident placeat, consequatur eveniet veritatis quos dignissimos beatae dolores exercitationem laboriosam officia magnam atque blanditiis illum doloremque magni ex corrupti tempora quis.
-                    </p>
-                </div>
-            </div>
-
-            <div>
-                <div className="inline-block p-3 text-white bg-black rounded-lg">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-                    </svg>
-                </div>
-
-                <div>
-                    <h1 className="text-xl font-semibold text-gray-700">Do i need a referral?</h1>
-
-                    <p className="mt-2 text-sm text-gray-500">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident placeat, consequatur eveniet veritatis quos dignissimos beatae dolores exercitationem laboriosam officia magnam atque blanditiis illum doloremque magni ex corrupti tempora quis.
-                    </p>
-                </div>
-            </div>
-
-            <div>
-                <div className="inline-block p-3 text-white bg-black rounded-lg">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-                    </svg>
-                </div>
-
-                <div>
-                    <h1 className="text-xl font-semibold text-gray-700">Is the cost of the appoinment covered by private health insurance?</h1>
-
-                    <p className="mt-2 text-sm text-gray-500">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident placeat, consequatur eveniet veritatis quos dignissimos beatae dolores exercitationem laboriosam officia magnam atque blanditiis illum doloremque magni ex corrupti tempora quis.
-                    </p>
-                </div>
-            </div>
-
-
-          
+            <p className="mt-2 text-sm text-gray-500 p-4">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident placeat, consequatur eveniet veritatis quos dignissimos beatae dolores exercitationem laboriosam officia magnam atque blanditiis illum doloremque magni ex corrupti tempora quis.
+            </p>
         </div>
-    </div>
+ 
+        <div>
+            <h1 className="text-xl font-semibold text-gray-700">What are your opening house?</h1>
+
+            <p className="mt-2 text-sm text-gray-500 p-4">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident placeat, consequatur eveniet veritatis quos dignissimos beatae dolores exercitationem laboriosam officia magnam atque blanditiis illum doloremque magni ex corrupti tempora quis.
+            </p>
+        </div>
+
+        <div>
+            <h1 className="text-xl font-semibold text-gray-700">Do i need a referral?</h1>
+
+            <p className="mt-2 text-sm text-gray-500 p-4">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident placeat, consequatur eveniet veritatis quos dignissimos beatae dolores exercitationem laboriosam officia magnam atque blanditiis illum doloremque magni ex corrupti tempora quis.
+            </p>
+        </div>
+
+        <div>
+            <h1 className="text-xl font-semibold text-gray-700">Is covered by private health insurance?</h1>
+
+            <p className="mt-2 text-sm text-gray-500 p-4">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident placeat, consequatur eveniet veritatis quos dignissimos beatae dolores exercitationem laboriosam officia magnam atque blanditiis illum doloremque magni ex corrupti tempora quis.
+            </p>
+            </div>
+        </div>
+      </div>
     </section>
   )
 }
