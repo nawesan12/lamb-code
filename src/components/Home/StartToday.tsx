a93044f2b113c60a7384570079c869bd92c75aec
Nahuel Santillan <nahueltecnicomdp@gmail.com>
2023-01-24 02:39:21 -0300

    SEO

--- src/components/Home/StartToday.tsx
+++ src/components/Home/StartToday.tsx
@@ -1,10 +1,9 @@
-import Link from "next/link";
-import GetStartedBtn from "../Buttons/GetStarted";
+import GetStartedBtn from "../Buttons/GetStarted"
 
 export default function HomeStartToday() {
   return (
     <section className="alter-bg py-20 flex flex-col items-center gap-10">
-      <h3 className="text-7xl font-bold">Start learning today</h3>
+      <h2 className="text-7xl font-bold">Start learning today</h2>
       <GetStartedBtn />
     </section>
   )
