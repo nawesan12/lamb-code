11081101f84d3a5354e139c4735e398733a28e2d
Nahuel Santillan <nahueltecnicomdp@gmail.com>
2023-01-24 23:57:37 -0300

    a

--- src/components/Modals/ArticlePromotion.tsx
+++ src/components/Modals/ArticlePromotion.tsx
@@ -2,10 +2,10 @@ import Link from "next/link"
 
 export default function ArticlePromotion({ link, blank }: { link: string, blank: boolean }) {
   return (
-    <section className="max-w-7xl mx-auto fixed bottom-6 left-6 z-50">
+    <section className="max-w-7xl mx-auto fixed bottom-6 left-6 z-50 cursor-pointer">
       <div className="relative group">
-        <div className="absolute -inset-1 bg-gradient-to-r from-purple-600 to-pink-600 rounded-lg blur opacity-25 group-hover:opacity-100 transition duration-1000 group-hover:duration-200"></div>
-        <div className="relative px-7 py-6 bg-white ring-1 ring-gray-900/5 rounded-lg leading-none flex items-top justify-start space-x-6">
+        <div className="absolute -inset-1 bg-gradient-to-r from-slate-200 to-indigo-300 rounded-lg blur opacity-25 group-hover:opacity-100 transition duration-1000 group-hover:duration-200"></div>
+        <div className="relative px-7 py-4 bg-white ring-1 ring-gray-900/5 rounded-lg leading-none flex items-top justify-start space-x-6">
           <svg className="w-8 h-8 text-purple-600" fill="none" viewBox="0 0 24 24">
             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M6.75 6.75C6.75 5.64543 7.64543 4.75 8.75 4.75H15.25C16.3546 4.75 17.25 5.64543 17.25 6.75V19.25L12 14.75L6.75 19.25V6.75Z"></path>
           </svg>
