ce8d9652ce3e66ca93b9e1516fd66555914f92c9
Nahuel Santillan <nahueltecnicomdp@gmail.com>
2023-01-24 23:47:34 -0300

    more mockup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,12 @@ import HomeHeader from "@/components/Home/Header"
 import HomeHero from "@/components/Home/Hero"
 import HomePricing from "@/components/Home/Pricing"
 import HomeStartToday from "@/components/Home/StartToday"
+import ArticlePromotion from "@/components/Modals/ArticlePromotion"
 
 export default function Home() {
   return (
     <main className="page">
+      <ArticlePromotion link="article" />
       <HomeHeader />
       <HomeHero />      
       <HomeFeatures />
--- src/components/Home/Header.tsx
+++ src/components/Home/Header.tsx
@@ -31,7 +31,7 @@ export default function HomeHeader() {
             </Link>
 
             <Link
-              className="rounded-lg bg-blue-600 px-5 py-2 text-sm font-medium text-white"
+              className="rounded-lg bg-black px-5 py-2 text-sm font-medium text-white"
               href="/auth/register"
             >
               Sign up
