f63db4bb09f7de7b72a91dd4e5eac960ab014cca
Nahuel Santillan <nahueltecnicomdp@gmail.com>
2023-01-26 23:23:10 -0300

    new section

--- src/components/Home/Stats.tsx
+++ src/components/Home/Stats.tsx
@@ -3,7 +3,7 @@ export default function HomeStats() {
     <section className="text-gray-600 body-font">
   <div className="container px-5 py-24 mx-auto">
     <div className="flex flex-col text-center w-full mb-20">
-      <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Master Cleanse Reliac Heirloom</h1>
+      <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Look at our statistics!</h1>
       <p className="lg:w-2/3 mx-auto leading-relaxed text-base">Learn whatever you want just to become the GOAT</p>
     </div>
     <div className="flex flex-wrap -m-4 text-center">
