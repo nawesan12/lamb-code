1af225d824e556b1e20e3615628465dee9fd74c9
Nahuel Santillan <nahueltecnicomdp@gmail.com>
2023-01-27 00:47:49 -0300

    for seo

--- src/components/Staff/Staff.tsx
+++ src/components/Staff/Staff.tsx
@@ -3,11 +3,11 @@ import Image from "next/image";
 export default function Staff() {
   return (
     <section className="text-gray-600 body-font">
-  <div className="container px-5 py-24 mx-auto">
-    <div className="flex flex-col text-center w-full mb-20">
-      <h1 className="text-4xl font-bold title-font mb-4 text-gray-900">THE TEAM</h1>
-      <p className="lg:w-2/3 mx-auto leading-relaxed text-base">Have you heard about them?</p>
-    </div>
+    <div className="container px-5 py-24 mx-auto">
+      <div className="flex flex-col text-center w-full mb-20">
+        <h1 className="text-4xl font-bold title-font mb-4 text-gray-900">THE TEAM</h1>
+        <p className="lg:w-2/3 mx-auto leading-relaxed text-base">Have you heard about them?</p>
+      </div>
     <div className="flex flex-wrap justify-evenly -m-4">
       
       <div className="p-4 lg:w-1/4 md:w-1/2">
@@ -20,7 +20,7 @@ export default function Staff() {
             <span className="inline-flex">
               <a className="text-gray-500">
                 <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
-                  <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+                  <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z" />
                 </svg>
               </a>
               <a className="ml-2 text-gray-500">
