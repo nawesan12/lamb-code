8d9c1ee88e91d18bd939514510c341886a13dc4d
Nahuel Santillan <nahueltecnicomdp@gmail.com>
2023-01-28 00:27:27 -0300

    a

--- src/pages/api/topics/[topic].ts
+++ src/pages/api/topics/[topic].ts
@@ -1,7 +1,12 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-export default function handler(req: NextApiRequest, res: NextApiResponse<string>) {
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
   const { topic } = req.query
 
-  res.status(200).json(topic as string)
+  if(!topic) {
+    return res.status(400).json({ error: "You did not send any topic to search!" })
+  }
+
+  res.status(200).json({ topic })
 }
+ 
