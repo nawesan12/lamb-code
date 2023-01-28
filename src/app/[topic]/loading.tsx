a0b18aeb7517cd1afc491331d93ff7510a85d2b4
Nahuel Santillan <nahueltecnicomdp@gmail.com>
2023-01-28 00:33:03 -0300

    a

--- src/app/[topic]/loading.tsx
+++ src/app/[topic]/loading.tsx
@@ -1,7 +1,11 @@
 export default function Loading() {
   return (
     <section className="section flex flex-col items-center justify-center">
-      <h1>Cargando...</h1>
+      <div className="flex justify-center items-center">
+        <div className="spinner-grow inline-block w-8 h-8 bg-current rounded-full opacity-0" role="status">
+          <span className="visually-hidden">Loading...</span>
+        </div>
+      </div>
     </section>
   )
 }
