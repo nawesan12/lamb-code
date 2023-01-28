c37383b8ec59be6b8955b4597fe05f456f0d82d4
Nahuel Santillan <nahueltecnicomdp@gmail.com>
2023-01-27 23:32:31 -0300

    a

--- src/app/terms-and-conditions/page.tsx
+++ src/app/terms-and-conditions/page.tsx
@@ -1,101 +1,55 @@
 export default function Page() {
   return (
-    <section className="page flex items-center justify-center">
-      <h1>TÉRMINOS Y CONDICIONES — Lamb Code</h1>
+    <section className="page flex flex-col items-center justify-center gap-7 text-justify py-12">
+      <h1 className="text-4xl font-bold">TÉRMINOS Y CONDICIONES — Lamb Code</h1>
 
-      <p>Lamb Code, persona jurídica de derecho privado describe, a través de este documento, las normas de uso del sitio https://lambcode.tech y de cualquier otro sitio web, tienda o aplicación operada por el titular.</p>
-      <p>Al navegar por este sitio web, consideramos que está de acuerdo con las condiciones de uso que figuran a continuación.</p>
-      <p>Si no está de acuerdo con los términos de este acuerdo, le pedimos que no haga ningún otro uso de este sitio web, mucho menos que se registre o envíe sus datos personales.</p>
-      <p>Si cambiamos nuestras condiciones de uso, publicaremos el nuevo texto en este sitio web, con una fecha de revisión actualizada. Podemos modificar este documento en cualquier momento.</p>    
-      <p>Si hay un cambio significativo en los términos de este acuerdo, podemos informarle utilizando la información de contacto que tenemos en nuestra base de datos o mediante una notificación.</p>
-      <p>El uso de este sitio web después de los cambios significa que usted acepta las condiciones de uso revisadas. Si, después de leer la versión revisada, no está de acuerdo con sus términos, por favor, termine su acceso.</p>
+      <section className="section lg:w-3/5 flex flex-col gap-4">
+        <p>Lamb Code, persona jurídica de derecho privado describe, a través de este documento, las normas de uso del sitio https://lambcode.tech y de cualquier otro sitio web, tienda o aplicación operada por el titular.</p>
+        <p>Al navegar por este sitio web, consideramos que está de acuerdo con las condiciones de uso que figuran a continuación.</p>
+        <p>Si no está de acuerdo con los términos de este acuerdo, le pedimos que no haga ningún otro uso de este sitio web, mucho menos que se registre o envíe sus datos personales.</p>
+        <p>Si cambiamos nuestras condiciones de uso, publicaremos el nuevo texto en este sitio web, con una fecha de revisión actualizada. Podemos modificar este documento en cualquier momento.</p>    
+        <p>Si hay un cambio significativo en los términos de este acuerdo, podemos informarle utilizando la información de contacto que tenemos en nuestra base de datos o mediante una notificación.</p>
+        <p>El uso de este sitio web después de los cambios significa que usted acepta las condiciones de uso revisadas. Si, después de leer la versión revisada, no está de acuerdo con sus términos, por favor, termine su acceso.</p>
 
-      <ul className="list-disc">
-        <li>Sección 1 - Usuario</li>
+        <ul className="flex justify-center flex-col gap-5">
+          <li className="font-bold text-xl">Sección 1 - Usuario</li>
 
-        <p>El uso de este sitio web le otorga automáticamente la condición de Usuario e implica su plena aceptación de todas las directrices y condiciones incluidas en estas Condiciones.</p>
+          <p>El uso de este sitio web le otorga automáticamente la condición de Usuario e implica su plena aceptación de todas las directrices y condiciones incluidas en estas Condiciones.</p>
 
-        <li>Sección 2 - Adhesión junto con la política de privacidad</li>
-        <p>El uso de este sitio web implica la adhesión a estas Condiciones de Uso y a la versión más actualizada de la Política de Privacidad de Lamb Code.</p>
+          <li className="font-bold text-xl">Sección 2 - Adhesión junto con la política de privacidad</li>
+          <p>El uso de este sitio web implica la adhesión a estas Condiciones de Uso y a la versión más actualizada de la Política de Privacidad de Lamb Code.</p>
 
-        <li>Sección 3 - Condiciones de acceso</li>
-        <p>En general, el acceso al sitio web de Lamb Code es gratuito y no requiere registro previo.</p>
-        <p>Sin embargo, para hacer uso de algunas funcionalidades, el usuario puede necesitar registrarse, creando una cuenta de usuario con un nombre de usuario y una contraseña de acceso.</p>
-        <p>Es responsabilidad del usuario proporcionar únicamente información correcta, auténtica, válida, completa y actualizada, así como no revelar su nombre de usuario y contraseña a terceros.</p>
-        <p>Algunas partes de este sitio web ofrecen al usuario la opción de publicar comentarios en determinadas áreas. Lamb Code no consiente la publicación de contenidos de carácter discriminatorio, ofensivo o ilícito, o que infrinjan los derechos de autor o cualquier otro derecho de terceros.</p>
-        <p>La publicación de cualquier contenido por parte del usuario de este sitio web, incluidos los mensajes y comentarios, implica una licencia no exclusiva, irrevocable e irreversible para su uso, reproducción y publicación por parte de Lamb Code en su sitio web, plataformas de internet y aplicaciones, o incluso en otras plataformas, sin ninguna restricción o limitación.</p>
+          <li className="font-bold text-xl">Sección 3 - Condiciones de acceso</li>
+          <p>En general, el acceso al sitio web de Lamb Code es gratuito y no requiere registro previo.</p>
+          <p>Sin embargo, para hacer uso de algunas funcionalidades, el usuario puede necesitar registrarse, creando una cuenta de usuario con un nombre de usuario y una contraseña de acceso.</p>
+          <p>Es responsabilidad del usuario proporcionar únicamente información correcta, auténtica, válida, completa y actualizada, así como no revelar su nombre de usuario y contraseña a terceros.</p>
+          <p>Algunas partes de este sitio web ofrecen al usuario la opción de publicar comentarios en determinadas áreas. Lamb Code no consiente la publicación de contenidos de carácter discriminatorio, ofensivo o ilícito, o que infrinjan los derechos de autor o cualquier otro derecho de terceros.</p>
+          <p>La publicación de cualquier contenido por parte del usuario de este sitio web, incluidos los mensajes y comentarios, implica una licencia no exclusiva, irrevocable e irreversible para su uso, reproducción y publicación por parte de Lamb Code en su sitio web, plataformas de internet y aplicaciones, o incluso en otras plataformas, sin ninguna restricción o limitación.</p>
 
-        <li>Sección 4 - Cookies</li>
-        <p>La información sobre el uso que usted hace de este sitio web puede recogerse a través de las cookies. Las cookies son piezas de información que se almacenan directamente en el equipo que está utilizando. Las cookies permiten recoger información como el tipo de navegador, el tiempo de permanencia en el sitio web, las páginas visitadas, las preferencias de idioma y otros datos de tráfico anónimos. Nosotros y nuestros proveedores de servicios utilizamos la información para la protección de la seguridad, para facilitar la navegación, mostrar la información de manera más eficiente y personalizar su experiencia al utilizar este sitio web, así como para el seguimiento en línea. También recopilamos información estadística sobre el uso del sitio web para mejorar continuamente nuestro diseño y funcionalidad, para entender cómo se utiliza el sitio web y para ayudarle a resolver problemas relevantes.</p>
-        <p></p>
-        <p></p>
+          <li className="font-bold text-xl">Sección 4 - Cookies</li>
+          <p>La información sobre el uso que usted hace de este sitio web puede recogerse a través de las cookies. Las cookies son piezas de información que se almacenan directamente en el equipo que está utilizando. Las cookies permiten recoger información como el tipo de navegador, el tiempo de permanencia en el sitio web, las páginas visitadas, las preferencias de idioma y otros datos de tráfico anónimos. Nosotros y nuestros proveedores de servicios utilizamos la información para la protección de la seguridad, para facilitar la navegación, mostrar la información de manera más eficiente y personalizar su experiencia al utilizar este sitio web, así como para el seguimiento en línea. También recopilamos información estadística sobre el uso del sitio web para mejorar continuamente nuestro diseño y funcionalidad, para entender cómo se utiliza el sitio web y para ayudarle a resolver problemas relevantes.</p>
+          <p>Si no desea que se recoja su información a través de las cookies, existe un procedimiento sencillo en la mayoría de los navegadores que permite rechazar automáticamente las cookies, o le da la opción de aceptar o rechazar la transferencia de una cookie específica (o varias) de un sitio web concreto a su ordenador. Sin embargo, esto puede crear inconvenientes en su uso del sitio web.</p>
+          <p>La configuración que elija puede afectar a su experiencia de navegación y al funcionamiento que requiere el uso de cookies. En este sentido, rechazamos cualquier responsabilidad por las consecuencias derivadas del funcionamiento limitado de este sitio web causado por la desactivación de las cookies en su dispositivo (incapacidad para establecer o leer una cookie).</p>
 
-        <li></li>
+          <li className="font-bold text-xl">Sección 5 - Propiedad intelectual</li>
+          <p>Todos los elementos de Lamb Code son propiedad intelectual de la misma o de sus licenciantes. Estos términos o el uso del sitio web no le otorgan ninguna licencia o derecho de uso de los derechos de propiedad intelectual de Lamb Code o de cualquier tercero.</p>
 
-        <li></li>
+          <li className="font-bold text-xl">Sección 6 - Enlaces a sitios web de terceros</li>
+          <p>Este sitio web puede contener, de vez en cuando, enlaces de hipertexto que le redirigirán a sitios web de nuestros socios, anunciantes, proveedores, etc. Si hace clic en uno de estos enlaces a cualquiera de estos sitios, recuerde que cada sitio tiene sus propias prácticas de privacidad y que nosotros no somos responsables de estas políticas. Por favor, consulte esas políticas antes de enviar cualquier dato personal a esos sitios.</p>
+          <p>No somos responsables de las políticas y prácticas de recopilación, uso y divulgación (incluidas las prácticas de protección de datos) de otras organizaciones, como Facebook, Apple, Google, Microsoft, o cualquier otro desarrollador de software o proveedor de aplicaciones, tienda de medios sociales, sistema operativo, proveedor de servicios de internet o fabricante de dispositivos, incluidos los Datos Personales que usted divulgue a otras organizaciones a través de las aplicaciones, en relación con dichas aplicaciones, o publicados en nuestras redes sociales. Le recomendamos que se informe sobre la política de privacidad y las condiciones de uso de cada sitio web visitado o proveedor de servicios utilizado.</p>
 
-        <li></li>
+          <li className="font-bold text-xl">Sección 7 - Plazos y modificaciones</li>
+          <p>El funcionamiento de este sitio web es por tiempo indefinido.</p>
+          <p>La totalidad del sitio web o cada una de sus secciones podrá ser cerrada, suspendida o interrumpida unilateralmente por Lamb Code, en cualquier momento y sin previo aviso.</p>
+          
 
-        <li></li>
+          <li className="font-bold text-xl">Sección 8 - Datos personales</li>
+          <p>Durante el uso de este sitio web, ciertos datos personales serán recogidos y procesados por Lamb Code y/o los Socios. Las normas relativas al tratamiento de los datos personales de Lamb Code están estipuladas en la Política de Privacidad.</p>
 
-        <li></li>
-
-      </ul>
-    
-
-    
-
-    - ****
-        
-        
-        
-    - ****
-        
-        
-        
-    - ****
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-    - ****
-        
-        
-        
-        Si no desea que se recoja su información a través de las cookies, existe un procedimiento sencillo en la mayoría de los navegadores que permite rechazar automáticamente las cookies, o le da la opción de aceptar o rechazar la transferencia de una cookie específica (o varias) de un sitio web concreto a su ordenador. Sin embargo, esto puede crear inconvenientes en su uso del sitio web.
-        
-        La configuración que elija puede afectar a su experiencia de navegación y al funcionamiento que requiere el uso de cookies. En este sentido, rechazamos cualquier responsabilidad por las consecuencias derivadas del funcionamiento limitado de este sitio web causado por la desactivación de las cookies en su dispositivo (incapacidad para establecer o leer una cookie).
-        
-    - **Sección 5 - Propiedad intelectual**
-        
-        Todos los elementos de Lamb Code son propiedad intelectual de la misma o de sus licenciantes. Estos términos o el uso del sitio web no le otorgan ninguna licencia o derecho de uso de los derechos de propiedad intelectual de Lamb Code o de cualquier tercero.
-        
-    - **Sección 6 - Enlaces a sitios web de terceros**
-        
-        Este sitio web puede contener, de vez en cuando, enlaces de hipertexto que le redirigirán a sitios web de nuestros socios, anunciantes, proveedores, etc. Si hace clic en uno de estos enlaces a cualquiera de estos sitios, recuerde que cada sitio tiene sus propias prácticas de privacidad y que nosotros no somos responsables de estas políticas. Por favor, consulte esas políticas antes de enviar cualquier dato personal a esos sitios.
-        
-        No somos responsables de las políticas y prácticas de recopilación, uso y divulgación (incluidas las prácticas de protección de datos) de otras organizaciones, como Facebook, Apple, Google, Microsoft, o cualquier otro desarrollador de software o proveedor de aplicaciones, tienda de medios sociales, sistema operativo, proveedor de servicios de internet o fabricante de dispositivos, incluidos los Datos Personales que usted divulgue a otras organizaciones a través de las aplicaciones, en relación con dichas aplicaciones, o publicados en nuestras redes sociales. Le recomendamos que se informe sobre la política de privacidad y las condiciones de uso de cada sitio web visitado o proveedor de servicios utilizado.
-        
-    - **Sección 7 - Plazos y modificaciones**
-        
-        El funcionamiento de este sitio web es por tiempo indefinido.
-        
-        La totalidad del sitio web o cada una de sus secciones podrá ser cerrada, suspendida o interrumpida unilateralmente por Lamb Code, en cualquier momento y sin previo aviso.
-        
-    - **Sección 8 - Datos personales**
-        
-        Durante el uso de este sitio web, ciertos datos personales serán recogidos y procesados por Lamb Code y/o los Socios. Las normas relativas al tratamiento de los datos personales de Lamb Code están estipuladas en la Política de Privacidad.
-        
-    - **Sección 9 - Contacto**
-        
-    Si tiene alguna pregunta sobre las condiciones de uso, por favor póngase en contacto con nosotros por correo electrónico lambcoding@gmail.com.
+          <li className="font-bold text-xl">Sección 9 - Contacto</li>
+          <p>Si tiene alguna pregunta sobre las condiciones de uso, por favor póngase en contacto con nosotros por correo electrónico lambcoding@gmail.com.</p>
+        </ul>
+      </section>
     </section>
   )
 }
--- src/components/Home/Hero.tsx
+++ src/components/Home/Hero.tsx
@@ -2,7 +2,7 @@ import GetStartedBtn from "../Buttons/GetStarted"
 
 export default function HomeHero() {
   return (
-    <section className="flex flex-col items-center justify-evenly h-4/5">
+    <section className="section flex flex-col items-center justify-evenly">
       <header>
         <h1 title="Lamb Code - The Lamb Project" className='text-7xl font-bold text-center whitespace-nowrap'>Lamb Code <br /> <small className="text-lg text-center block drop-shadow-sm shadow-green-600">The Lamb Project</small> </h1>
       </header>
