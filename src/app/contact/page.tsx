991230921d7c2bd5933d8da705882321c04ed3e4
Nahuel Santillan <nahueltecnicomdp@gmail.com>
2023-01-27 23:33:04 -0300

    a

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -15,7 +15,7 @@ export default function Page() {
               </div>
               <div className="sm:col-span-2">
                   <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900">Your message</label>
-                  <textarea id="message" rows={6} className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500" placeholder="Leave a comment"></textarea>
+                  <textarea id="message" rows={6} className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 resize-none" placeholder="Leave a comment"></textarea>
               </div>
               <button type="submit" className="py-3 px-5 text-sm font-medium text-center text-black rounded-lg bg-white border-2 border-black sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300">Send message</button>
           </form>
