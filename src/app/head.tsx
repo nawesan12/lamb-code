e62193a686b610e1a4c90642485a7230ae63141d
Nahuel Santillan <nahueltecnicomdp@gmail.com>
2023-01-30 20:09:43 -0300

    new title

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,7 +1,7 @@
 export default function Head() {
   return (
     <>
-      <title>The Lamb Project - Lamb Code</title>
+      <title>Lamb Code | Where the knowledge begins</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Lamb Code is an educational, collaborating and helpful page for developers and people that wants to start his career in IT." />
       <link rel="icon" href="/favicon.ico" />
