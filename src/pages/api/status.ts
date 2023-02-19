811fae7892db14e6eadec3b9578a10a2ee1a4b67
Nahuel Santillan <nahueltecnicomdp@gmail.com>
2023-02-18 23:56:24 -0300

    a

--- src/pages/api/status.ts
+++ src/pages/api/status.ts
@@ -3,5 +3,7 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 export default function handler(_: NextApiRequest, res: NextApiResponse<string>) {
   const status: string = "🟢 All systems stable" 
 
+  res.setDefaultEncoding("utf-8")
+
   res.status(200).send(status)
 }
